e889f35d62ca4e2829c21534a3eeee6247b7a98f
Sergey Kochetkov <skochetkov@Sergeys-MacBook-Pro.local>
2018-04-12 15:34:22 -0400

    removed some js files

--- src/main/resources/static/js/app/app.js
+++
@@ -1,7 +0,0 @@
-angular.module('changeExample', [])
-    .controller('ExampleController', ['$scope', function($scope) {
-      $scope.counter = 0;
-      $scope.change = function() {
-        $scope.counter++;
-      };
-    }]);
